635a3c5412117bc0e8ee94500676391b7bdb4af9
laisiewcheng <laisiewcheng@hotmail.com>
2020-05-06 13:08:41 +0800

    Add files via upload

--- resnet_deform.py
+++ resnet_deform.py
@@ -4,8 +4,12 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 import sys
+import os
+import shutil
 
 import deform_conv
+
+f1 = open("output-resnet-size.txt", "a")
 
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride, padding=1, bias=True)
@@ -62,25 +66,42 @@ class Bottleneck(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=True)
         self.bn1 = nn.BatchNorm2d(planes)
         #apply deformable layer here as specified in the paper
-        #self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
-        self.conv2 = deform_conv.DeformConv2D(planes, planes, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+        #self.conv2 = deform_conv.DeformConv2D(planes, planes, kernel_size=3, padding=1)
         self.bn2 = nn.BatchNorm2d(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=True)
         self.bn3 = nn.BatchNorm2d(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
+        #if stride used is not 1 (but 2 like in layer 2 and 3), 
+        #then need to perform convolution of 1 x 1 first before shortcut connection
+        #OR if input filter (in_planes) not equal to output filter (self.expansion*planes), 
+        #then also need to perform convolution of 1 x 1 first before shortcut connection to make the
+        #dimension match in summation
         if stride != 1 or in_planes != self.expansion*planes:
+            print('stride: ', stride)
+            print('in_planes: ', in_planes, '   self.expansion*planes: ', self.expansion*planes)
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=True),
                 nn.BatchNorm2d(self.expansion*planes)
             )
 
     def forward(self, x):
+        print('*************************START******************************')
+        print('x: ', x.size(), file = f1)
         out = F.relu(self.bn1(self.conv1(x)))
+        print('out1: ', out.size(), file = f1)
         out = F.relu(self.bn2(self.conv2(out)))
+        print('out2: ', out.size(), file = f1)
         out = self.bn3(self.conv3(out))
+        print('out3: ', out.size(), file = f1)
+        print('x2: ', x.size(), file = f1)
         out += self.shortcut(x)
+        print('out4: ', out.size(), file = f1)
+        print('x3: ', x.size())
         out = F.relu(out)
+        print('out5: ', out.size(), file = f1)
+        print('***************************END******************************')
 
         return out
 
@@ -95,11 +116,18 @@ class ResNet(nn.Module):
         print('block: ', block)
         print('num_blocks: ', num_blocks)
 
+        
         self.conv1 = conv3x3(3,16)
+       
         self.bn1 = nn.BatchNorm2d(16)
+        
+        
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
+         
         self.layer2 = self._make_layer(block, 32, num_blocks[1], stride=2)
+         
         self.layer3 = self._make_layer(block, 64, num_blocks[2], stride=2)
+        
         self.linear = nn.Linear(64*block.expansion, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
@@ -113,10 +141,19 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
+        print('START RESNET 101')
+        print('input x to network: ', x.size())
         out = F.relu(self.bn1(self.conv1(x)))
+        print('out1 resnet: ', out)
+        print('START LAYER 1 WITH 16 FILTER')
         out = self.layer1(out)
+        print('out2 from layer 1: ', out)
+        print('START LAYER 2 WITH 32 FILTER')
         out = self.layer2(out)
+        print('out3 from layer 2: ', out)
+        print('START LAYER 3 WITH 64 FILTER')
         out = self.layer3(out)
+        print('out4 from layer 3: ', out)
         out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
